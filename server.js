b5098679188b0d20e58838ad88f50ebc5f1376d1
etuong <etuong@gmail.com>
2022-06-21 14:05:01 -0700

    Added Supertest and Jest

--- server.js
+++ server.js
@@ -9,18 +9,22 @@ const app = new Koa();
 const router = new Router();
 
 router.get('/', async ctx => {
-    const name = ctx.query.name || 'stranger';
-    ctx.set('Content-Type', 'text/html')
-    ctx.body = `<h1>Hello ${name}!</h1>`;
+  const name = ctx.query.name || 'stranger';
+  ctx.set('Content-Type', 'text/html')
+  ctx.status = 200;
+  ctx.body = `<h1>Hello ${name}!</h1>`;
 });
 
 router.get('/secret', async ctx => {
-    ctx.redirect('test.html')
+  ctx.redirect('test.html')
 })
 
 app
-    .use(bodyParser())
-    .use(router.routes())
-    .use(router.allowedMethods())
-    .use(koaStatic('./pages'))
-    .listen(PORT, '0.0.0.0', () => console.log(`listening on http://localhost:${PORT}...`));
+  .use(bodyParser())
+  .use(router.routes())
+  .use(router.allowedMethods())
+  .use(koaStatic('./pages'));
+
+const server = app.listen(PORT, '0.0.0.0', () => console.log(`listening on http://localhost:${PORT}...`));
+
+module.exports = server;
---
+++ test/server.test.js
@@ -0,0 +1,20 @@
+const request = require("supertest");
+const app = require("../server");
+
+describe("Server", () => {
+  it("page should return hello world by default", async () => {
+    const response = await request(app).get('/');
+    expect(response.statusCode).toEqual(200);
+    expect(response.text).toEqual("<h1>Hello stranger!</h1>");
+  });
+
+  it("page should return hello {name} if name query exists", async () => {
+    const response = await request(app).get('/').query({ name: 'Ethan' });
+    expect(response.statusCode).toEqual(200);
+    expect(response.text).toEqual("<h1>Hello Ethan!</h1>");
+  });
+});
+
+afterAll(() => {
+  app.close();
+});

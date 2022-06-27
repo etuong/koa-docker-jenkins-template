d8feedf3794730a93b89963e02d8769d3e204f7b
etuong <etuong@gmail.com>
2022-06-27 09:37:14 -0700

    Test Jenkins

--- test/server.test.js
+++ test/server.test.js
@@ -11,7 +11,7 @@ describe("Server", () => {
   it("page should return hello {name} if name query exists", async () => {
     const response = await request(app).get('/').query({ name: 'Ethan' });
     expect(response.statusCode).toEqual(200);
-    expect(response.text).toEqual("<h1>Hello Mary!</h1>");
+    expect(response.text).toEqual("<h1>Hello Ethan!</h1>");
   });
 });
 
